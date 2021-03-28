36639bbba222ebbbf061eac623460f03449eae2b
thutham <62605290+thutham@users.noreply.github.com>
2021-03-28 18:15:11 +0700

    Add onTopButton

--- myscript.js
+++ myscript.js
@@ -1,4 +1,17 @@
-< script >
+console.log("JS run success")
+const clickEvent = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
 
-    <
-    /script>
+const scrollFunc = () => {
+    if (window.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementsByClassName("topButton")[0].style.display = "flex"
+    } else {
+        document.getElementsByClassName("topButton")[0].style.display = "none"
+    }
+}
+window.onscroll = scrollFunc
+document.getElementsByClassName("topButton")[0].addEventListener("click", clickEvent)
